929ba27efe5000f9e7e3ddbe15708f7f5d4926c7
Arman Ansari <eternal00112@gmail.com>
2026-03-16 14:56:09 +0530

    string freq explained

--- Strings/stringsfre.java
+++ Strings/stringsfre.java
@@ -5,10 +5,13 @@ public class stringsfre {
     
      
      String str="xxxbbbccccddyyyy";
-      int fre[]=new int[26];
+      int fre[]=new int[26];     //starting me sabki i.e;indexx 0 se lekar 25 indx tk sbki vslue 0 hogi.
+      //  and jab bhi koi character aayega to uski value 1 ho jayegi. and agar same character aaya to uski value 2
+      //  ho jayegi and is tarah se count karte jayenge.
       for(int i=0;i<str.length();i++)
       {
-          fre[str.charAt(i)-'a']++;
+          fre[str.charAt(i)-'a']++;  //isme char ko int me convert karne ke liye 'a' se minus karte hai. kyuki a=0, b=1, c=2, d=3 and so on.
+          //joki index ko denote krta h aur jb bhi o index dubara milta value increases..
       }
       char ch='$';
       int max=fre[0];
