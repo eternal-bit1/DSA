44b8cdf7292839c759f15be0dd454b4361ff5653
Arman Ansari <eternal00112@gmail.com>
2026-03-16 14:39:51 +0530

    optimal approach for maxiimumsubarray

--- Strings/kadane.java
+++ Strings/kadane.java
@@ -1,20 +1,44 @@
-public class kadane {
+
+     //bruteforce approach
+
     
+//     public int maxSubArray(int[] nums) {
+//         int n=0;
+//          for(int i=0;i<nums.length;i++)
+//         {
+//             for(int j=i;j<nums.length;j++)
+//             {
+//                 int sum =0;
+//                 for(int k =i;k<=j;k++)
+//                 {
+//                     sum=sum+nums[k];
+//                     n=Math.max(sum,n);
+//                 }
+//             }
+//         }
+//         return n;
+//     }
+// }
+ 
+ 
+                    //    OPTIMAL APPROACH
+
+public class kadane {
+    class Solution {
     public int maxSubArray(int[] nums) {
-        int n=0;
-         for(int i=0;i<nums.length;i++)
+        int sum=0;
+        int maximumsum=nums[0];
+        for(int i=0;i<nums.length;i++)
         {
-            for(int j=i;j<nums.length;j++)
+            sum+=nums[i];
+            maximumsum=Math.max(maximumsum,sum);
+            if (sum<0)
             {
-                int sum =0;
-                for(int k =i;k<=j;k++)
-                {
-                    sum=sum+nums[k];
-                    n=Math.max(sum,n);
-                }
+                sum=0;
             }
+          
         }
-        return n;
+        return maximumsum;
     }
 }
- 
+}
