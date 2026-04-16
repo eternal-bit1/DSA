d6319052fc7d0f5eee5a5885b10ebdc242fe2928
ARMAN ANSARI <eternal00112@gmail.com>
2026-04-17 02:29:24 +0530

    Enhance word counting logic with comments
    Added comments to clarify the logic for counting words.

--- Strings/wordcount.java
+++ Strings/wordcount.java
@@ -6,7 +6,9 @@ public class wordcount {
     boolean inWord = false;
 
     for (int i = 0; i < s.length(); i++) {
-        if (s.charAt(i) != ' ' && !inWord) {
+        if (s.charAt(i) != ' ' && !inWord) //here it checks that the character is inside of word or not because it is boolean and the if coondition block will excute only 
+        //both the condition is true and here it totally depends on inword whether it is true or false
+        {
             count++;
             inWord = true;
         } else if (s.charAt(i) == ' ') {
